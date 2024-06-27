b3733d6934aacf66323ef87bd169c4537651564e
darudeeee <haode0916@naver.com>
2024-06-27 17:21:27 +0900

    :(

--- back/server.js
+++ back/server.js
@@ -7,11 +7,12 @@ const db = require("./DataBase/db");
 const app = express();
 
 // Body Parser 설정
-app.use(express.json({ limit: "10mb" })); // JSON 파싱 제한 설정
-app.use(express.urlencoded({ extended: true, limit: "10mb" })); // URL 인코딩 파싱 제한 설정
+app.use(express.json({ limit: "50mb" })); // JSON 파싱 제한 설정
+app.use(express.urlencoded({ extended: true, limit: "50mb" })); // URL 인코딩 파싱 제한 설정
+app.use(express.raw({ limit: "50mb", type: "application/json" }));
 
 // 요청 헤더의 최대 크기 설정 추가
-app.use(express.raw({ limit: "10mb", type: "application/json" }));
+app.use(express.raw({ limit: "50mb", type: "application/json" }));
 
 // Middleware 설정
 app.use(cors()); // CORS 설정
@@ -26,8 +27,17 @@ app.use((err, req, res, next) => {
   res.status(500).send("서버 오류 발생!");
 });
 
+// 클라이언트 에러 처리
+app.on("clientError", (err, socket) => {
+  socket.end("HTTP/1.1 431 Request Header Fields Too Large\r\n\r\n");
+});
+
 // 서버 시작
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT} 포트에서 실행 중입니다.`);
-});
+app
+  .listen(PORT, () => {
+    console.log(`서버가 http://localhost:${PORT} 포트에서 실행 중입니다.`);
+  })
+  .on("clientError", (err, socket) => {
+    socket.end("HTTP/1.1 431 Request Header Fields Too Large\r\n\r\n");
+  });
